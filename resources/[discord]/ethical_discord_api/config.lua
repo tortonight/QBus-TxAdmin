504b3a5a03c3487c9ff5cf500adc611bdc63181d
tortonight <90068041+tortonight@users.noreply.github.com>
2022-01-13 18:54:27 +0800

    Create config.lua

---
+++ resources/[discord]/ethical_discord_api/config.lua
@@ -0,0 +1,19 @@
+Config = {
+	Guild_ID = '881265605557231697',
+	Bot_Token = 'ODgxMjYyNTcwNzI2NjUzOTcy.YSqRuQ.RzLQZCXQi0UmId4rj_SIhL2HB6o',
+	RoleList = {
+		['Moderator'] = 927586179367469107,
+		['Admin'] = 927592193500250113,
+		['owner'] = 927592399117647933,
+	},
+}
+
+Config.Splash = {
+	Header_IMG = 'https://cdn.discordapp.com/attachments/927584488354111558/927584701751914547/logo_tonight2.png',
+	Enabled = false,
+	Wait = 12,
+	Heading1 = "ยินดีต้อนรับสู่ | TORUNIT | TONIGHT CITY RP",
+	Heading2 = "อย่าลืมเข้าร่วม Discord ของเราและอ่านกฎเพื่อความปลอดภัย!",
+	Discord_Link = 'https://discord.gg/xBHRKFpNNP',
+	Website_Link = 'https://www.torunit.net/',
+}
