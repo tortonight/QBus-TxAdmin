1066e21d3af18e42fcc6dd72aa2eaedefa70c1f3
tortonight <90068041+tortonight@users.noreply.github.com>
2022-01-13 18:56:43 +0800

    Create config.lua

---
+++ resources/[discord]/EthicalSplash/config.lua
@@ -0,0 +1,10 @@
+Config = {
+	Server_Name = "| TORUNIT | TONIGHT CITY RP",
+	Discord_Link = 'https://discord.gg/xBHRKFpNNP',
+	Website_Link = 'https://www.torunit.net/',
+	roleList = {
+		{927586179367469107, "group.trialstaff"}, --[[ Moderator --- ]]
+		{927592193500250113, "group.serverstaff"}, --[[ Admin --- ]]
+		{927592399117647933, "group.owner"}, --[[ Owner --- ]]
+	},
+}
